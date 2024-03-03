8c351aa0b592b959b634fea6c6a9622c0d8d5b5b
rodioncikita <chikita.rodion@gmail.com>
2024-03-03 16:40:27 +0300

    1

--- L25-di/homework-template/build.gradle.kts
+++ L25-di/homework-template/build.gradle.kts
@@ -4,5 +4,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.assertj:assertj-core")
+    implementation("ch.qos.logback:logback-classic")
     testImplementation("org.mockito:mockito-junit-jupiter")
 }
--- L25-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L25-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,11 +1,18 @@
 package ru.otus.appcontainer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
+    private static final Logger logger = LoggerFactory.getLogger(AppComponentsContainerImpl.class);
 
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
@@ -16,7 +23,22 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        var methods = List.of(configClass.getDeclaredMethods());
+        List <Method> sortedMethods = methods.stream().sorted(Comparator.comparingInt(x -> x.getAnnotation(AppComponent.class).order())).toList();
+        for (Method method : sortedMethods){
+            try {
+                List<Class<?>> params  =  List.of(method.getParameterTypes());
+                List<Object> objects = new ArrayList<>();
+                for (Class<?> param : params){
+                    objects.add(getAppComponent(param));
+                }
+                Object component = method.invoke(configClass.getConstructor().newInstance(), objects.toArray());
+                appComponents.add(component);
+                appComponentsByName.put(method.getAnnotation(AppComponent.class).name(), component);
+            } catch (Throwable ex){
+                logger.info(String.valueOf(ex.getCause()));
+            }
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -26,12 +48,19 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(Class<C> componentClass) {
+        for (Object component : appComponents){
+            if (componentClass.isInstance(component)){
+                return (C) component;
+            }
+         }
         return null;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
